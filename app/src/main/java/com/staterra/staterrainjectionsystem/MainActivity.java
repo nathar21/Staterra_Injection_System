1e7b296d984b95da79d2f588ed3ee12d03fefa3e
nathar21 <nataliaharris23@gmail.com>
2015-01-04 18:15:21 -0800

    New Layout buttons and screens

--- app/src/main/java/com/staterra/staterrainjectionsystem/MainActivity.java
+++ app/src/main/java/com/staterra/staterrainjectionsystem/MainActivity.java
@@ -1,17 +1,43 @@
 package com.staterra.staterrainjectionsystem;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 //testing lola begs too good
 public class MainActivity extends ActionBarActivity {
+
+    Button page1;
+    Button page2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //SetMainPage();
+        createButtons();
+    }
+
+    private void createButtons(){
+        page1 = (Button)findViewById(R.id.button);
+        page2 = (Button)findViewById(R.id.button2);
+
+        page1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent myIntent = new Intent(MainActivity.this, Current_Environmental_Status.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+        page2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent myIntent = new Intent(MainActivity.this, Data_Exportation.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 
 
